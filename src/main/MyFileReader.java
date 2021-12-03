768680b567243ea54eed1e8a2d25f9e4db6b11dc
Omer Kacar <omerkacar1@gmail.com>
2021-12-03 16:53:28 +0000

    test(core): add MyFileReaderTest.java
    Tests if all the data points are correct

--- src/main/MyFileReader.java
+++ src/main/MyFileReader.java
@@ -31,8 +31,11 @@ public class MyFileReader {
 	            while (fileInput.hasNext()) {
 					String regex = "\\s+"; // split all space occurrences in the line.
 	                String[] splitLine = fileInput.nextLine().split(regex);
-	                int dataX = Integer.parseInt(splitLine[2].trim()); // adds point x
-	                int dataY = Integer.parseInt(splitLine[3].trim()); // add point y
+
+					double[] dataPointsXAndY = getXandY(splitLine); //index 0 is x value, index 1 is y value
+
+	                double dataX = dataPointsXAndY[0]; // gets point x
+	                double dataY = dataPointsXAndY[1]; // gets point y
 	                dataPoints[pointNumber] = new DataPoint(dataX, dataY); // creates an instance of the class DataPoint with dataX and dataY.
 	                pointNumber++;
 	            }
@@ -44,9 +47,33 @@ public class MyFileReader {
 
 	        return dataPoints;
 	    }
-	    
 
-	    /**
+
+		/**
+		 * Gets x and y coordinates while avoiding the index number (first number in the line)
+		 * @param splitLine		a line containing the index - x coordinate - y coordinate
+		 * @return				an array containing the x coordinate at index 0 and y coordinate at index 1
+		 */
+		private double[] getXandY(String[] splitLine) {
+			double[] xAndY = new double[2];
+			int xyIterator = 0; //at 0 x will be initialised, at 1 y will be initialised
+			boolean passedIndex = false; //ensures index is not taken as an x or y coordinate.
+
+			for(String partOfLine : splitLine) {
+				partOfLine = partOfLine.trim();
+				if(partOfLine.length() > 0) {
+					if(passedIndex) {
+						xAndY[xyIterator] = Double.parseDouble(partOfLine);
+						xyIterator++;
+					} else
+						passedIndex = true;
+				}
+			}
+			return xAndY;
+		}
+
+
+	/**
 	     * counts the number of lines (data points) in the DATA_FILE
 	     *
 	     * @return the number of lines in the DATA_FILE, which is the number of data points.
--- src/main/MyFileReaderTest.java
+++ src/main/MyFileReaderTest.java
@@ -1,28 +1,37 @@
 package main;
 
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNot;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyFileReaderTest {
 
-//    @Test
-//    public void testTrainProblem1() {
-//        final String filePath = System.getProperty("user.dir") + "/Resources/trainProblem1"; //absolute path to the file
-//
-//        MyFileReader myFileReader = new MyFileReader(filePath);
-//
-//        DataPoint[] dataPoints = myFileReader.getData();
-//
-//        System.out.println(MyArrays.toString(dataPoints));
-//    }
-//
-//    @Test
-//    public void testTrainProblem1() {
-//        final String filePath = System.getProperty("user.dir") + "/Resources/trainProblem1"; //absolute path to the file
-//
-//        MyFileReader myFileReader = new MyFileReader(filePath);
-//
-//        DataPoint[] dataPoints = myFileReader.getData();
-//
-//        System.out.println(MyArrays.toString(dataPoints));
-//    }
+    @ParameterizedTest
+    @ValueSource(strings = {"trainProblem1.txt", "trainProblem2.txt", "trainProblem3.txt", "test1_2018.txt",
+            "test2_2018.txt", "test3_2018.txt", "test4_2018.txt", "test1_2019.txt", "test2_2019.txt", "test3_2019.txt",
+            "test4_2019.txt", "test1_2020.txt", "test2_2020.txt", "test3_2020.txt", "test4_2020.txt"})
+    public void testIndexIsNotAssignedAsX(String filename) {
+        final String filePath = System.getProperty("user.dir") + "/Resources/" + filename; //absolute path to the file
+        System.out.println(filePath);
+        MyFileReader myFileReader = new MyFileReader(filePath);
+
+        DataPoint[] dataPoints = myFileReader.getData();
+        System.out.println(MyArrays.toString(dataPoints));
+        int numberOfPoints = dataPoints.length;
+
+        double[] xValues = new double[numberOfPoints];
+        double[] expecteds = new double[numberOfPoints];
+
+        for(int pointIterator = 0; pointIterator < dataPoints.length; pointIterator++) {
+            xValues[pointIterator] = dataPoints[pointIterator].getX();
+            expecteds[pointIterator] = pointIterator+1;
+        }
+
+        Assert.assertThat(xValues, IsNot.not(IsEqual.equalTo(expecteds)));
+    }
 }
